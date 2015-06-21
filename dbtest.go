b9dfe194c4693878284587d83ec4e548bf845b57
David Skeppstedt <david.skeppstedt@gmail.com>
2015-06-21 22:34:32 +0200

    Reading the db configuration from a credentials.txt file which is not stored in version controle due to saftey

--- dbtest.go
+++ dbtest.go
@@ -1,24 +1,51 @@
 package main
 
 import (
+	//"bufio"
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"io/ioutil"
 	"log"
+	"strings"
 )
 
 var (
 	db *sql.DB
 )
 
+//init - Init metod run before main.
 func init() {
+	/*
+	*credentials.txt must have the following structure to work:
+	*host_ip
+	*port
+	*user
+	*password
+	*dbname
+	*sslmode*/
+	credentials, ferr := ioutil.ReadFile("credentials.txt")
+	check(ferr)
+	config := strings.Split(string(credentials), "\n")
+
+	dbConfig := "host=" + config[0] +
+		" port=" + config[1] +
+		" user=" + config[2] +
+		" password=" + config[3] +
+		" dbname=" + config[4] +
+		" sslmode=" + config[5]
+
 	var err error
-	db, err = sql.Open("postgres", "host=192.168.1.5 port=5432 user=postgres password=**** dbname=login_prototype sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
+	db, err = sql.Open("postgres", dbConfig)
+	check(err)
 }
+
+//main - when go run dbtest.go is invoked this method is run.
 func main() {
+	selectAllUsers(db)
+}
+
+func selectAllUsers(db *sql.DB) {
 
 	rows, err := db.Query("SELECT * FROM users")
 	if err != nil {
@@ -30,5 +57,10 @@ func main() {
 		rows.Scan(&in, &st)
 		fmt.Printf("%s %d \n", st, in)
 	}
+}
 
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
 }
