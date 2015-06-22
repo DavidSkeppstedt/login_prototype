7d26f234fa0a92f04246c195a458c402eb687c81
David Skeppstedt <david.skeppstedt@gmail.com>
2015-06-22 22:32:17 +0200

    First draft of the api handlers

--- server_martini.go
+++ server_martini.go
@@ -8,9 +8,38 @@ func main() {
 		return "Hello World!"
 	})
 	m.Get("/api/v1", apiV1)
+
+	m.Group("/api/v1/user", func(r martini.Router) {
+		r.Get("/:id", GetUser)
+		r.Get("/list/all", ListUsers)
+		r.Post("/new/", NewUser)
+		r.Put("/update/:id", UpdateUser)
+		r.Delete("/delete/:id", DeleteUser)
+	})
+
 	m.Run()
 }
 
 func apiV1() string {
 	return "Hello API!"
+}
+
+func NewUser(params martini.Params) (int, string) {
+	return 200, "Added a new user!"
+}
+
+func ListUsers() (int, string) {
+	return 501, "501 - Not Implemented"
+}
+
+func GetUser(params martini.Params) (int, string) {
+	return 200, "{user{id:" + params["id"] + ",name:'David'}}"
+}
+
+func UpdateUser(params martini.Params) (int, string) {
+	return 501, "501 - Not Implemented"
+}
+
+func DeleteUser(params martini.Params) (int, string) {
+	return 501, "501 - Not Implemented"
 }
